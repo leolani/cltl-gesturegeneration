8c755d0acbad4c3a78ca8064ff1fc403304ac191
Mark Adamik <m.adamik@vu.nl>
2022-04-29 12:57:19 +0200

    Added an emotion dictionary, need to connect to the emotion module, test on robot

--- src/cltl/gesture_generation/pepper_gesturer.py
+++ src/cltl/gesture_generation/pepper_gesturer.py
@@ -6,6 +6,8 @@ class PepperGesturer(Gesturer):
     def __init__(self):
         """
         Create a gesture for the Pepper robot based on an incoming emotion
+        The complete list of readily available gestures are available at:
+        http://doc.aldebaran.com/2-5/naoqi/motion/alanimationplayer-advanced.html#animationplayer-list-behaviors-pepper
 
         Parameters
         ----------
