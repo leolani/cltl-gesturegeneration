d9b63fec09f115a491dead9c317243d496f675eb
Mark Adamik <m.adamik@vu.nl>
2022-04-29 13:00:17 +0200

    added gesture dictionary, needs testing

--- src/cltl/gesture_generation/pepper_gesturer.py
+++ src/cltl/gesture_generation/pepper_gesturer.py
@@ -1,5 +1,5 @@
 from cltl.gesture_generation.api import Gesturer
-
+import random
 
 class PepperGesturer(Gesturer):
 
@@ -16,7 +16,26 @@ class PepperGesturer(Gesturer):
         super(Gesturer, self).__init__()
 
     def generate_gesture(self, emotion):
-        # TODO: Mark magic!
-        gesture = None
+        """
+        Matches the emotion to a gesture.
+        The current emotions supported are:
+            "neutral"
+            "joy"
+            "surprise"
+            "anger"
+            "sadness"
+            "disgust"
+            "fear"
+        """
+        gesture_dic = {
+            "neutral" : ["animations/Stand/Gestures/Explain_1"]
+            "joy" : ["animations/Stand/Emotions/Positive/Happy_4"]
+            "surprise" : ["animations/Stand/Emotions/Positive/Hysterical_1", "animations/Stand/Gestures/Excited_1"]
+            "anger" : ["animations/Stand//Gestures/CalmDown_" + str(x) for x in [1, 5, 6]]
+            "sadness" : ["animations/Stand/Gestures/Desperate_" + str(x) for x in [1, 2, 4, 5]]
+            "disgust" : ["animations/Stand/Emotions/Negative/Bored_1"]
+            "fear" : ["animations/Stand//Gestures/CalmDown_" + str(x) for x in [1, 5, 6]]
+        }
+        gesture = random.choice(gesture_dic[emotion])
 
         return gesture
